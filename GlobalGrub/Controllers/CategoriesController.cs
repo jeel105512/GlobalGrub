441354a00522903ca09ad60ed27f3760e78eedbf
JeelKumar Patel <jeelhpatel10@gmail.com>
2023-05-10 14:49:47 -0400

    hardcoded category list to select

---
+++ GlobalGrub/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalGrub.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // /categories/browse?category=abc - show selected category
+        public IActionResult Browse(string category)
+        {
+            // store input parameter in a var inside a ciewbag so we can display the user's selection
+            ViewBag.category = category;
+
+            return View();
+        }
+    }
+}
