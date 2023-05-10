e8b7d43283bda90efbc6c515ad8a2d764a515512
JeelKumar Patel <jeelhpatel10@gmail.com>
2023-05-10 16:47:05 -0400

    mock category list with our model

--- GlobalGrub/Controllers/CategoriesController.cs
+++ GlobalGrub/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GlobalGrub.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GlobalGrub.Controllers
 {
@@ -6,12 +7,29 @@ namespace GlobalGrub.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            // use the Category model to create 10 categories object in memory
+            // List<Category> categories = new List<Category>();
+            var categories = new List<Category>();
+            
+            for(var i = 1; i < 11; i++)
+            {
+                categories.Add(new Category() { CategoryId = i, Name = "Category " + i.ToString() });
+            }
+
+            // pass the categories list as strongly-typed data to the view for display
+            return View(categories);
         }
 
         // /categories/browse?category=abc - show selected category
         public IActionResult Browse(string category)
         {
+            // ensure we have a category value
+            if(category == null)
+            {
+                // redirect the user to the Index so thay can choose a category
+                return RedirectToAction("Index");
+            }
+
             // store input parameter in a var inside a ciewbag so we can display the user's selection
             ViewBag.category = category;
 
---
+++ GlobalGrub/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace GlobalGrub.Models
+{
+    public class Category
+    {
+        // all pk ields ASP.NET MVC should be called either {Model}Id or Id
+        // properties names should always be PascalCase
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
