70da3d7d3eedba52ff05c5895fb6e8374a564cba
JeelKumar Patel <jeelhpatel10@gmail.com>
2023-05-12 17:11:04 -0400

    lesson 03 Part 1 - category view model anotations or validations

--- GlobalGrub/Controllers/CategoriesController.cs
+++ GlobalGrub/Controllers/CategoriesController.cs
@@ -35,5 +35,11 @@ namespace GlobalGrub.Controllers
 
             return View();
         }
+
+        // /Categories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
--- GlobalGrub/Models/Category.cs
+++ GlobalGrub/Models/Category.cs
@@ -1,10 +1,17 @@
-﻿namespace GlobalGrub.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GlobalGrub.Models
 {
     public class Category
     {
         // all pk ields ASP.NET MVC should be called either {Model}Id or Id
         // properties names should always be PascalCase
+        
+        [Range(1, 9999999)]
+        [Display(Name = "Category Id")] // this sets an alias for all labels globally
         public int CategoryId { get; set; }
+
+        [Required(AllowEmptyStrings =  false, ErrorMessage = "And no empty string ther")]
         public string Name { get; set; }
     }
 }
